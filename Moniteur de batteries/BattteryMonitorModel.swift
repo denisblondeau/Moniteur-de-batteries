5f48fe962d30509e64d2c0b536adee7d0305b5aa
Denis Blondeau <dbappdev@outlook.com>
2022-10-27 03:44:05 -0400

    Class IOEventDetector completed

---
+++ Moniteur de batteries/BattteryMonitorModel.swift
@@ -0,0 +1,12 @@
+//
+//  BattteryMonitorModel.swift
+//  Moniteur de batteries
+//
+//  Created by Denis Blondeau on 2022-10-27.
+//
+
+import Foundation
+
+final class BattteryMonitorModel: ObservableObject {
+    
+}
