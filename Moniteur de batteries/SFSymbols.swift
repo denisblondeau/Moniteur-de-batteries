485c83bdeaf64578cda43e10024326cd81b7aed7
Denis Blondeau <dbappdev@outlook.com>
2022-10-27 03:54:11 -0400

    Import SF Symbols

---
+++ Moniteur de batteries/SFSymbols.swift
@@ -0,0 +1,18 @@
+//
+//  SFSymbols.swift
+//  Moniteur de batteries
+//
+//  Created by Denis Blondeau on 2022-10-27.
+//
+
+import Foundation
+
+enum SFSymbol: String {
+    case keyboard = "􀇳"
+    case magicmouse = "􀺰"
+    case batteryLevel0 = "􀛪"
+    case batteryLevel25 = "􀛩"
+    case batteryLevel50 = "􀺶"
+    case batteryLevel75 = "􀺸"
+    case batteryLevel100 = "􀛨"
+}
