245ffd5fdb708203a91de32d7994b19eedd5265d
Denis Blondeau <dbappdev@outlook.com>
2022-10-26 21:37:35 -0400

    Initial Commit

---
+++ Moniteur de batteries/Moniteur_de_batteriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moniteur_de_batteriesApp.swift
+//  Moniteur de batteries
+//
+//  Created by Denis Blondeau on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct Moniteur_de_batteriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
